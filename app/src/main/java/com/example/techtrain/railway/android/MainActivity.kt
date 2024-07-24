66652144379e89e6a211f87af07ab0c49e778685
K.Shimpo <k022c0054@g.neec.ac.jp>
2024-07-24 17:30:56 +0900

    add button

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.techtrain.railway.android
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<View>(R.id.btShow)
+        button.setOnClickListener(ButtonClickListener())
+    }
+
+    inner class ButtonClickListener : View.OnClickListener {
+        override fun onClick(v: View) {
+            val editText = findViewById<EditText>(R.id.editTextText)
+            val textView = findViewById<TextView>(R.id.text)
+            textView.text = editText.text.toString()
+        }
     }
 }
