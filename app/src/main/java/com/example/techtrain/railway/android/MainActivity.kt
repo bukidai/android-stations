ec3de913ef1366cd2c74e8887eb7dad12df81b77
K.Shimpo <k022c0054@g.neec.ac.jp>
2024-07-31 15:08:08 +0900

    add SecondIntent

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -2,23 +2,18 @@ package com.example.techtrain.railway.android
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.techtrain.railway.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var _binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(_binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button = _binding.btShow
-        button.setOnClickListener(ButtonClickListener())
-    }
-
-    inner class ButtonClickListener : View.OnClickListener {
-        override fun onClick(v: View) {
+        val button = binding.btShow
+        button.setOnClickListener() {
             val intent = Intent(this@MainActivity, SecondActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/techtrain/railway/android/SecondActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/SecondActivity.kt
@@ -2,10 +2,13 @@ package com.example.techtrain.railway.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.techtrain.railway.android.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
